f31c1236165e614ced4bdd23458d82f1665d3794
commvnist <commvnist@gmail.com>
2026-08-30 14:22:55 -0400

    release: prepare 0.3.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "commvne.com.whip.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 31
-        versionName = "0.3.25"
+        versionCode = 32
+        versionName = "0.3.26"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
