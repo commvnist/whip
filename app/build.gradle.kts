07e5c708d12ebe98ca7671bfa57e57faf5fd345f
commvnist <commvnist@gmail.com>
2026-09-07 15:02:56 -0400

    Prepare Whip 0.3.61 private release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "commvne.com.whip.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 66
-        versionName = "0.3.60"
+        versionCode = 67
+        versionName = "0.3.61"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
